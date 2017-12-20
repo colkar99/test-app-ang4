2997b8be393fd547031b699756317c5b8ee9cec1
colkar99 <colkar99@gmail.com>
2017-12-20 22:48:15 +0530

    first tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  name = 'karthik';
+  employees=[
+			{name : 'Karthik',
+			dob : '21-2-1992',
+			age: 25,
+			place : 'trichy'
+			},
+			{name : 'lalitha',
+			dob : '27-6-1991',
+			age: 26,
+			place : 'musiri'
+			},
+			{name : 'senthil',
+			dob : '27-6-1990',
+			age: 27,
+			place : 'dubai'
+			}   
+  ];
+  averageAge(){
+  let sum = 0;
+  	for (let emp of this.employees){
+  		sum += emp.age ;
+  	}
+  	return sum;
+  }
 }
